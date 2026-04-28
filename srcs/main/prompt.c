814b6f227f008f3aa181180172c3624c539aa207
blukerin <blukerin82@gmail.com>
2026-04-28 19:54:11 +0200

    Prompt mejorado

--- srcs/main/prompt.c
+++ srcs/main/prompt.c
@@ -12,7 +12,7 @@
 
 #include "../../includes/minishell.h"
 
-static int	get_prompt_home(char *env_home, char **line, char *dir)
+static int	get_prompt_home(char *env_home, char **line, char *dir, t_sh *mini)
 {
 	int		l;
 	char	*prompt;
@@ -21,14 +21,18 @@ static int	get_prompt_home(char *env_home, char **line, char *dir)
 
 	env = env_home + 5;
 	l = ft_strlen(env);
-	size = (ft_strlen(dir) - l) + 54;
+	size = (ft_strlen(dir) - l) + 100;
 	prompt = malloc(sizeof(char) * size);
 	if (!prompt)
 		exit (1);
-	ft_strlcpy(prompt, "\001\033[1;32m\002➜  \001\033[1;36m\002minishell:\001\033[94m\002", size);
+	ft_strlcpy(prompt, "\001\033[1;32m\002➜  ", size);
+	ft_strlcpy(prompt, "\001\033[1;36m\002minishell:\001\033[94m\002", size);
 	ft_strlcat(prompt, " ~", size);
 	ft_strlcat(prompt, dir + l, size);
-	ft_strlcat(prompt, "\001\033[0m\002 ❯ ", size);
+	if (mini->exit_status == 0)
+		ft_strlcat(prompt, "\001\033[1;32m\002 ❯ \001\033[0m\002", size);
+	else
+		ft_strlcat(prompt, "\001\033[1;31m\002 ❯ \001\033[0m\002", size);		
 	(*line) = readline(prompt);
 	free(prompt);
 	return (1);
@@ -56,10 +60,27 @@ static void	search_home(char **env, char *dir, int *i)
 	return ;
 }
 
+static void	get_prompt_no_home(t_sh *mini, char **line, char *dir)
+{
+	char	*prompt;
+	int		size;
+
+	size = (ft_strlen(dir)) + 100;
+	prompt = malloc(sizeof(char) * size);
+	ft_strlcpy(prompt, "\001\033[1;32m\002➜  ", size);
+	ft_strlcpy(prompt, "\001\033[1;36m\002minishell:\001\033[94m\002", size);
+	ft_strlcat(prompt, dir, size);
+	if (mini->exit_status == 0)
+		ft_strlcat(prompt, "\001\033[1;32m\002 ❯ \001\033[0m\002", size);
+	else
+		ft_strlcat(prompt, "\001\033[1;31m\002 ❯ \001\033[0m\002", size);
+	(*line) = readline(prompt);
+	free(prompt);
+}
+
 void	get_prompt(t_sh *mini, char **line)
 {
 	char	dir[PATH_MAX];
-	char	*prompt;
 	int		i;
 
 	i = 0;
@@ -69,17 +90,14 @@ void	get_prompt(t_sh *mini, char **line)
 		return ;
 	}
 	search_home(mini->envp, dir, &i);
-	if (mini->envp[i] && get_prompt_home(mini->envp[i], line, dir))
-		return ;
-	prompt = malloc(sizeof(char) * (ft_strlen(dir) + 52));
-	if (!prompt)
+	if (mini->envp[i])
 	{
-		free_matrix(mini->envp);
-		exit(1);
+		get_prompt_home(mini->envp[i], line, dir, mini);
+		return ;
 	}
-	ft_strlcpy(prompt, "\001\033[1;32m\002➜  \001\033[1;36m\002minishell:\001\033[94m\002", 42);
-	ft_strlcat(prompt, dir, (ft_strlen(dir) + 42));
-	ft_strlcat(prompt, "\001\033[0m\002 ❯ ", (ft_strlen(prompt) + 13));
-	(*line) = readline(prompt);
-	free(prompt);
+	else
+	{
+		get_prompt_no_home(mini, line, dir);
+		return ;
+	}
 }
